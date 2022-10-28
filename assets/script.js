4f8a46e4bf39157cce89c26b692aceebae900de1
Cisco Ferre <ferrecisco@icloud.com>
2022-10-28 08:34:06 -0400

    JS link query

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+// Defining constant variables
+var OPEN_WEATHER_API_KEY = "49160125f6ab2cf7e612c72cdd9d0e00";
+var CITY_SEARCH_INPUT = document.querySelector("#city-search");
+var CITY_SEARCH_BUTTON = document.querySelector("#search-button");
+var CITY_NAME = document.querySelector("#city-name");
+var CITY_ICON = document.querySelector("#title-img");
+var TEMP = document.querySelector("#temp");
+var WIND = document.querySelector("#wind");
+var HUMIDITY = document.querySelector("#humidity");
+var UV_INDEX = document.querySelector("#uv-index");
+var RECENT_SEARCHES = document.querySelector("#recent-searches");
+var FIVE_DAY_CONTAINER = document.querySelector("#five-day-container")
+var Moment = moment();
+
